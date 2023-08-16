6508fee83746f96e6a412afa7df669597ae0a2f2
lzeek <86994603+lzeek@users.noreply.github.com>
2023-08-16 18:10:07 +0200

    rename vars
    renamed some variables for to better understand app logic

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
-let dayInputValue = document.getElementById("day");
-let monthInputValue = document.getElementById("month");
-let yearInputValue = document.getElementById("year");
+let dayInput = document.getElementById("day");
+let monthInput = document.getElementById("month");
+let yearInput = document.getElementById("year");
 let dateInput = document.querySelectorAll("[require]");
 let getAgeBtn = document.getElementById("new-age");
 let showYear = document.getElementById("years--show");
@@ -12,9 +12,9 @@ let yearError = document.getElementById("year--error");
 let dateError = document.querySelectorAll(".error--text");
 
 //make sure inputs are numbers
-const dayInputIntValue = Number(dayInputValue.value);
-const monthInputIntValue = Number(monthInputValue.value);
-const yearInputIntValue = Number(yearInputValue.value);
+const dayInputIntValue = Number(dayInput.value);
+const monthInputIntValue = Number(monthInput.value);
+const yearInputIntValue = Number(yearInput.value);
 
 // get present date and store variables
 const currentDate = new Date();
@@ -22,15 +22,22 @@ const currentDay = currentDate.getDate();
 const currentMonth = currentDate.getMonth() + 1;
 const currentYear = currentDate.getFullYear();
 
+dayInput.addEventListener("input", (e) => {
+  let dayInputValue = e.target.value;
+  console.log(dayInputValue);
+});
+
 getAgeBtn.addEventListener("click", () => {
   if (
-    dayInputValue.value === "" &&
-    monthInputValue.value === "" &&
-    yearInputValue.value === ""
+    dayInput.value === "" &&
+    monthInput.value === "" &&
+    yearInput.value === ""
   ) {
-    console.log("Hey baby");
     dateError.forEach((element) => {
       element.classList.add("error--text--show");
+      dayError.textContent = "This field is recquired";
+      monthError.textContent = "This field is recquired";
+      yearError.textContent = "This field is recquired";
     });
   }
 });
